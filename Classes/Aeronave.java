1c2027d16a8691b4701106db1a07117a6564da78
fhssouza <fhssouza@gmail.com>
2021-06-30 22:43:35 -0300

    Avaliação 01 POO

---
+++ Classes/Aeronave.java
@@ -0,0 +1,22 @@
+package Classes;
+
+public class Aeronave extends Pessoa {
+    private int matricula;
+    private String habilitacao;
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getHabilitacao() {
+        return habilitacao;
+    }
+
+    public void setHabilitacao(String habilitacao) {
+        this.habilitacao = habilitacao;
+    }
+}
